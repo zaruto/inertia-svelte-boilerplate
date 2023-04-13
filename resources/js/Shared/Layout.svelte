4d37b79bdb47fd8458bf0da03e7c8b647d103a05
Zaruto <iamismailzaid@gmail.com>
2023-04-14 01:50:36 +0500

    animate added

--- resources/js/Shared/Layout.svelte
+++ resources/js/Shared/Layout.svelte
@@ -1,6 +1,14 @@
-
 <script>
+
+   import { fade } from 'svelte/transition';
+
    export let name;
+
+   let isActive = false;
+
+   function toggleActive() {
+    isActive = !isActive;
+  }
 </script>
 
 <div>
@@ -298,7 +306,7 @@
 
             <!-- Profile dropdown -->
             <div class="relative">
-              <button type="button" class="-m-1.5 flex items-center p-1.5" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+              <button on:click={toggleActive} type="button" class="-m-1.5 flex items-center p-1.5" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                 <span class="sr-only">Open user menu</span>
                 <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                 <span class="hidden lg:flex lg:items-center">
@@ -319,12 +327,20 @@
                   From: "transform opacity-100 scale-100"
                   To: "transform opacity-0 scale-95"
               -->
-              <div class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                <!-- Active: "bg-gray-50", Not Active: "" -->
-                <a href="#" class="block px-3 py-1 text-sm leading-6 text-gray-900" role="menuitem" tabindex="-1" id="user-menu-item-0">Your profile</a>
 
-                <a href="#" class="block px-3 py-1 text-sm leading-6 text-gray-900" role="menuitem" tabindex="-1" id="user-menu-item-1">Sign out</a>
-              </div>
+              {#if isActive}
+
+                <div transition:fade class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                    <!-- Active: "bg-gray-50", Not Active: "" -->
+                    <a href="/" class="block px-3 py-1 text-sm leading-6 text-gray-900" role="menuitem" tabindex="-1" id="user-menu-item-0">Your profile</a>
+
+                    <a href="/" class="block px-3 py-1 text-sm leading-6 text-gray-900" role="menuitem" tabindex="-1" id="user-menu-item-1">Sign out</a>
+                </div>
+
+              {/if}
+
+
+
             </div>
           </div>
         </div>
